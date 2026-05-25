76b693826af9caf1e6db5d71026d95da9881a735
SuperSaiyanShark <lonielrana@gmail.com>
2026-05-25 12:41:04 +0800

    added ziggy.js and routes to help

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,6 +4,10 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import route from 'ziggy-js'; // 💡 Add the core Ziggy function package
+
+// 💡 Attach route to the global window scope immediately
+window.route = route;
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
